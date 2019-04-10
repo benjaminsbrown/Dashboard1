639428964f98569f3d6dfb8056c5fca21d3b755e
Benjamin Brown <35267836+benjaminsbrown@users.noreply.github.com>
2019-04-10 16:36:44 +0100

    revert previous escape

--- server2.php
+++ server2.php
@@ -17,8 +17,8 @@ if (isset($_POST['reg_staff'])) {
   $Contract_hours = mysqli_real_escape_string($db, $_POST['Contract_hours']);
   $Actual_hours = mysqli_real_escape_string($db, $_POST['Actual_hours']);
   $Employment = mysqli_real_escape_string($db, $_POST['Employment']);
-  $Start_date = mysqli_connect($db, $_POST['Start_date']);
-  $End_date = mysqli_connect($db, $_POST['End_date']);
+  $Start_date = mysqli_real_escape_string($db, $_POST['Start_date']);
+  $End_date = mysqli_real_escape_string($db, $_POST['End_date']);
 
 
   if (empty($Name)) { array_push($errors, "Name is required"); }
