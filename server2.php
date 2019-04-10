0e4ebb3915ea1b6b979193a8733bd11a3a65a931
Benjamin Brown <35267836+benjaminsbrown@users.noreply.github.com>
2019-04-10 17:02:10 +0100

    removed )

--- server2.php
+++ server2.php
@@ -14,11 +14,11 @@ $db = mysqli_connect('localhost', 'root', '', 'dashboard1');
 //Adding staff members
 if (isset($_POST['reg_staff'])) {
   $Name = $_POST['Name']);
-  $Contract_hours = $_POST['Contract_hours']);
-  $Actual_hours = $_POST['Actual_hours']);
-  $Employment = $_POST['Employment']);
-  $Start_date = $_POST['Start_date']);
-  $End_date = $_POST['End_date']);
+  $Contract_hours = $_POST['Contract_hours'];
+  $Actual_hours = $_POST['Actual_hours'];
+  $Employment = $_POST['Employment'];
+  $Start_date = $_POST['Start_date'];
+  $End_date = $_POST['End_date'];
 
 
   if (empty($Name)) { array_push($errors, "Name is required"); }
