42b378fde8bd734d6e7be7b3516ce84a1acb6a96
Benjamin Brown <35267836+benjaminsbrown@users.noreply.github.com>
2019-04-10 17:57:44 +0100

    Update server2.php

--- server2.php
+++ server2.php
@@ -11,7 +11,27 @@ $errors = array();
 
 $db = mysqli_connect('localhost', 'root', '', 'dashboard1');
 
+if (isset($_POST['reg_staff'])) {
+  $name = mysqli_real_escape_string($db, $_POST['name']);
+  $contract = mysqli_real_escape_string($db, $_POST['contract']);
+  $worked = mysqli_real_escape_string($db, $_POST['worked']);
+  $employment = mysqli_real_escape_string($db, $_POST['employment']);
+  $started = mysqli_real_escape_string($db, $_POST['started']);
+  $ended = mysqli_real_escape_string($db, $_POST['ended']);
 
+
+  if (empty($name)) { array_push($errors, "name is required"); }
+  if (empty($contract)) { array_push($errors, "Contract hours is required"); }
+  if (empty($employment)) { array_push($errors, "employment is required"); }
+  if (empty($started)) { array_push($errors, "Start date is required"); }
+
+  if (count($errors) == 0) {
+
+
+  	$query = "INSERT INTO Staff2 (name, contract, worked, employment, started, ended)
+          VALUES ('$name', '$contract', '$worked', '$employment', '$started', '$ended')";
+  }
+}
 
 
 ?>
