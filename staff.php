7d99fd8f027b12dc6dcd8f605355e981235213e6
Benjamin Brown <35267836+benjaminsbrown@users.noreply.github.com>
2019-04-18 14:48:24 +0100

    .

--- staff.php
+++ staff.php
@@ -142,12 +142,7 @@ if (isset($_GET['logout'])) {
       <div class="container-fluid">
 
         <!-- Breadcrumbs-->
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="#">Dashboard</a>
-          </li>
-          <li class="breadcrumb-item active">Overview</li>
-        </ol>
+        
 
 
 <html>
